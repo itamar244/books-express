d0dda3a3c62cd12e8a32c94401012209ab54f574
Itamar Yatom <itamary24@gmail.com>
2021-06-25 16:37:01 +0300

    Update CartButton.jsx

--- src/components/TopBar/CartButton/CartButton.jsx
+++ src/components/TopBar/CartButton/CartButton.jsx
@@ -1,3 +1,5 @@
+// CR: this component doesn't feel like it belongs to TopBar, because it actually controls the cart context.
+// either move it to components, or another state if `cartModal` should be open or not then put `CartModal` in `App`
 import React, { useState, useContext } from "react";
 
 import { Button, Badge, Typography } from "@material-ui/core";
@@ -8,19 +10,24 @@ import CartContext from "../../../store/cart/cart-context";
 
 import cartButtonStyles from "./CartButton.styles";
 
+// CR: Component has both logic and styling. This is considered as bad practice
 const CartButton = () => {
   const classes = cartButtonStyles();
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const cartCtx = useContext(CartContext);
 
+  // CR: shouldn't be a func :( you just call it later.
+  // wrap it with `useMemo` for saving extra computations
   const getBookInCart = () =>
     cartCtx.items.reduce((quantity, item) => (quantity += item.amount), 0);
 
+  // CR: useCallback
   const openCartHandler = () => {
     setIsModalOpen(true);
   };
 
+  // CR: useCallback
   const closeCartHandler = () => {
     setIsModalOpen(false);
   };
