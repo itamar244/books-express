43c242c476e43eb686ad4a1d5da2f1bcc9f85159
Itamar Yatom <itamary24@gmail.com>
2021-06-25 14:21:34 +0300

    Update cart-reducer.js

--- src/store/cart/cart-reducer.js
+++ src/store/cart/cart-reducer.js
@@ -1,11 +1,17 @@
 import { ADD_ITEM, REMOVE_ITEM } from "./actions";
 
+// CR: cartReducer's state could be just an item array, I would prefer it that way.
+// If you need other fields you can create other reducers and providers, 
+// and this will be better because reducers are decoupled
 export const initialState = {
   items: [],
 };
 
 export const cartReducer = (state, action) => {
+  // CR: switch..case is a convention for reducers
+  // CR: you can do `case ADD_ITEM: return addItemToState(state, action)` if you don't like bloated cases
   if (action.type === ADD_ITEM) {
+    // CR: just noting, new item is actually already a clone because you clone it in BookItemForm
     const newItem = { ...action.item };
 
     const existingItemIndex = state.items.findIndex(
@@ -14,6 +20,7 @@ export const cartReducer = (state, action) => {
 
     if (existingItemIndex !== -1) {
       const updatedItems = [...state.items];
+      // CR: you need another shallow clone to book, `updatedItems[existingItemIndex]= newItem`
       updatedItems[existingItemIndex].amount += newItem.amount;
       return {
         ...state,
@@ -27,6 +34,7 @@ export const cartReducer = (state, action) => {
     };
   }
 
+  // CR: logic inside if is nicely done :)
   if (action.type === REMOVE_ITEM) {
     const itemIdToRemove = action.itemId;
 
@@ -54,5 +62,6 @@ export const cartReducer = (state, action) => {
     };
   }
 
+  // CR: badddd, return state, that way if there's no updates react can tell it can escape a rerender
   return { ...state };
 };
