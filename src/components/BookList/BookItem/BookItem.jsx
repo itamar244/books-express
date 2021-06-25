afd6b1d5083fb5e1407f329f0bbd3b49be0e0b08
Itamar Yatom <itamary24@gmail.com>
2021-06-25 16:29:03 +0300

    Update BookItem.jsx

--- src/components/BookList/BookItem/BookItem.jsx
+++ src/components/BookList/BookItem/BookItem.jsx
@@ -10,6 +10,10 @@ import bookItemStyles from "./BookItem.styles";
 const BookItem = ({ book }) => {
   const classes = bookItemStyles();
 
+  // CR: I have personal preference, that wrapping components like ListItem, Card etc. should be used in parent,
+  // and component like `BookItem` would be without wrapping components.
+  // then you can use `BookItem` in different contexts without the fear that you would need another wrapper.
+  // You could use `<></>` to do it, but `<section class="book-item" />` would look better in the DOM
   return (
     <ListItem divider className={classes.root}>
       <BookItemInfo book={book} />
